77e4a749e7924ea278c9715d7043f155d40a24c7
Louis Qian <yuq4@illinois.edu>
2022-12-27 03:31:42 -0600

    Formatting & Linting

--- lua/louis/plugins-setup.lua
+++ lua/louis/plugins-setup.lua
@@ -71,11 +71,12 @@ return packer.startup(function(use)
   use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
   use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
   use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
-  
+
+  -- formatting & linting
+  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
   if packer_bootstrap then
     require("packer").sync()
   end
-  
 end)
-  
-  
